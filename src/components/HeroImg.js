a05529fa0303ccae01b37a5b5b4e3bcff6562475
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-02-07 15:48:50 -0300

    developing components and routes

---
+++ src/components/HeroImg.js
@@ -0,0 +1,26 @@
+import './HeroImg.css';
+
+import React from 'react';
+
+import introImg from "../assets/intro-bg.jpg";
+import { Link } from 'react-router-dom';
+
+const HeroImg = () => {
+  return (
+    <div className='hero'>
+        <div className='mask'>
+            <img className="intro-img" src={introImg} alt="background"/>
+        </div>
+        <div className="content">
+            <p>HI, I'M A FREELANCER.</p>
+            <h1>REACT DEVELOPER</h1>
+            <div>
+                <Link to="/projects" className='btn'>Projects</Link>
+                <Link to="/contact" className='btn btn-light'>Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg
