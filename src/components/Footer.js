6e5c3df32efdde0aeab7ff751fb47356d6ad63f1
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-02-20 22:54:32 -0300

    adding countries-app to projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Footer.css';
 
-import { FaGithub, FaHome, FaLinkedin, FaMailBulk, FaPhone } from 'react-icons/fa';
+import { FaGithub, FaHome, FaLinkedin, FaMailBulk } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -15,10 +16,6 @@ const Footer = () => {
                       <p>Argentina</p>
                     </div>
                 </div>
-                <div className='phone'>
-                  <h4><FaPhone size={20} style={{color: '#fff', marginRight: '2rem'}} />
-                  +542236877187</h4>                  
-                </div>
                 <div className='mail'>
                   <h4><FaMailBulk size={20} style={{color: '#fff', marginRight: '2rem'}} />
                   ivanconsortework@gmail.com</h4>                  
@@ -26,10 +23,14 @@ const Footer = () => {
             </div>
             <div className='right'>
               <h4>About me</h4>
-              <p>Hello! I'm Ivan Consorte and this is my portfolio</p>
+              <p>Hello! I'm Ivan Consorte and this is my portfolio: a place where I showcase my works</p>
               <div className='social'>
-                <FaGithub size={30} style={{color: '#fff', marginRight: '1rem'}} />
-                <FaLinkedin size={30} style={{color: '#fff', marginRight: '1rem'}} />
+                <NavLink to='https://github.com/IvanConsorteWork/CountriesApp'>
+                  <FaGithub size={30} style={{color: '#fff', marginRight: '1rem'}} />
+                </NavLink>
+                <NavLink to='https://www.linkedin.com/in/ivan-consorte-b4a772249/'>
+                  <FaLinkedin size={30} style={{color: '#fff', marginRight: '1rem'}} />
+                </NavLink>
               </div>
             </div>
         </div>
--- src/components/WordCardData.js
+++ src/components/WordCardData.js
@@ -1,27 +1,14 @@
-import pro1 from '../assets/pro1.png';
+import countries from '../assets/countries-pic.png';
 
 const WorkCardData = [
     {
-        imgsrc: pro1,
+        imgsrc: countries,
         title: "Countries App",
-        text: "A Full Stack project for developing skills as web developer. This site acts as a database",
-        view: "" 
+        text: "A Full Stack project for developing skills as web developer. This site acts as a countries's database",
+        view: "https://countries-app-ivan-consorte.vercel.app/",
+        source: "https://github.com/IvanConsorteWork/CountriesApp"
         // Acá iria la dirección del sitio web adecuado
     },
-    {
-        imgsrc: pro1,
-        title: "Countries App",
-        text: "A Full Stack project for developing skills as web developer. This site acts as a database",
-        view: "" 
-        // Acá iria la dirección del sitio web adecuado
-    },
-    {
-        imgsrc: pro1,
-        title: "Countries App",
-        text: "A Full Stack project for developing skills as web developer. This site acts as a database",
-        view: "" 
-        // Acá iria la dirección del sitio web adecuado
-    }
 ]
 
 export default WorkCardData;
