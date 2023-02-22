0991eca2f41e3e298aaee2519ac866a791643187
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-02-22 16:37:07 -0300

    fixing hamburguer CSS

--- src/components/WordCardData.js
+++ src/components/WordCardData.js
@@ -3,9 +3,9 @@ import countries from '../assets/countries-pic.png';
 const WorkCardData = [
     {
         imgsrc: countries,
-        title: "Countries App",
+        title: "Terralog",
         text: "A Full Stack project for developing skills as web developer. This site acts as a countries's database",
-        view: "https://countries-app-ivan-consorte.vercel.app/",
+        view: "https://terralog.vercel.app/",
         source: "https://github.com/IvanConsorteWork/CountriesApp"
         // Acá iria la dirección del sitio web adecuado
     },
