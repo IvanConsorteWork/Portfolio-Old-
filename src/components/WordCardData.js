a9c91a89e470bfbbbf41bb01bad070cd116b5d73
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-03-05 23:13:40 -0300

    adding becoming-fit to projects

--- src/components/WordCardData.js
+++ src/components/WordCardData.js
@@ -1,13 +1,20 @@
 import countries from '../assets/countries-pic.png';
+import becomingfit from '../assets/becoming-fit.png';
 
 const WorkCardData = [
     {
         imgsrc: countries,
         title: "Terralog",
-        text: "A Full Stack project for developing skills as web developer. This site acts as a countries's database",
+        text: "A Full Stack project for developing skills as web developer. This site acts as a countries's database. Includes search, filters, sorts and content creation",
         view: "https://terralog.vercel.app/",
         source: "https://github.com/IvanConsorteWork/CountriesApp"
-        // Acá iria la dirección del sitio web adecuado
+    },
+    {
+        imgsrc: becomingfit,
+        title: "Becoming-Fit",
+        text: "A responsive Full Stack sports outfit's e-commerce project. Includes authentication, shopping cart/checkout and admin functions.",
+        view: "https://becoming-fit-br1oli.vercel.app/",
+        source: "https://github.com/IvanConsorteWork/Becoming_Fit"
     },
 ]
 
